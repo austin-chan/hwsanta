4525f8eefefe9c37f37fe91e6f87fec8f8b9d1e3
Austin Chan <achan1@MBA.local>
2012-11-24 21:30:24 -0800

    Initial Commit

---
+++ SadnessHole/AppViewController.h
@@ -0,0 +1,13 @@
+//
+//  AppViewController.h
+//  SadnessHole
+//
+//  Created by Austin Chan on 11/24/12.
+//  Copyright (c) 2012 Austin Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppViewController : UIViewController
+
+@end
---
+++ SadnessHole/AppViewController.m
@@ -0,0 +1,29 @@
+//
+//  AppViewController.m
+//  SadnessHole
+//
+//  Created by Austin Chan on 11/24/12.
+//  Copyright (c) 2012 Austin Chan. All rights reserved.
+//
+
+#import "AppViewController.h"
+
+@interface AppViewController ()
+
+@end
+
+@implementation AppViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
